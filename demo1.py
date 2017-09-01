b801091cade987af76daa7b45d4dc227f9778f03
DRL-PC\DRL <drl1116@163.com>
2017-09-01 19:49:02 +0800

    一个小测试题目

--- demo1.py
+++ demo1.py
@@ -1,5 +1,16 @@
 # -*- coding: utf-8 -*-
 print('Hello World')
 
+for i in range(0, 100):
+    i = i + 1
+    if i % 15 == 0:
+        print("FizzBuzz")
+    elif i % 3 == 0:
+        print("Fizz")
+    elif i % 5 == 0:
+        print("Buzz")
+    else:
+        print(i)
+
 a = input("请输入：")
 print(a)
